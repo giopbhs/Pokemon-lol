2168cda9b7543e5deb207f8a6c76b1bef29663b7
Giovani Camara <Gcamara14@gmail.com>
2016-06-28 22:36:32 -0700

    Add devise and pins user association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,7 +9,7 @@ class PinsController < ApplicationController
 	end
 
 	def create
-		@pin = Pin.new(pin_params)
+		@pin = current_user.pins.build(pin_params)
 
 		if @pin.save
 			redirect_to @pin, notice: "Succesfully Created new Pin"
@@ -19,7 +19,7 @@ class PinsController < ApplicationController
 	end
 
 	def new
-		@pin = Pin.new
+		@pin = current_user.pins.build
 	end
 
 	def edit
