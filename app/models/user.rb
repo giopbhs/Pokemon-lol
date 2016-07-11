b92d3732a93ca1e3cd09b96a411e2318b1a26755
Giovani Camara <Gcamara14@gmail.com>
2016-07-10 20:55:20 -0700

    adds comments to links

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   has_many :links
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :comments
   resources :links do
   	member do
   		put "like",    to: "links#upvote"
   		put "dislike", to: "links#downvote"
   	end
+    resources :comments
   end
 
   devise_for :users
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
   		put "like", to: "pins#upvote"
   		put "dislike", to: "pins#downvote"
   	end
+    resources :comments
   end
 
   root "pins#index"
