92cab2afd09f80de705584604a13fd9c0dd8ab60
Giovani Camara <Gcamara14@gmail.com>
2016-07-10 18:38:59 -0700

    authorization on links

--- db/migrate/20160710071709_add_user_id_to_links.rb
+++ db/migrate/20160710071709_add_user_id_to_links.rb
@@ -1,6 +1,8 @@
 class AddUserIdToLinks < ActiveRecord::Migration
   def change
-    add_column :links, :user_id, :integer
+    add_column :links, :user_id, :integer, nil: false
     add_index :links, :user_id
   end
 end
+
+
